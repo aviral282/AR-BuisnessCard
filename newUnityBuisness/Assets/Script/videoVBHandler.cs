22045fb79a7ea001a22c42e28b3befdcdedb3abd
Aviral Rajput <aviral282@gmail.com>
2019-04-10 20:45:25 +0100

    Commit1

---
+++ newUnityBuisness/Assets/Script/videoVBHandler.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class videoVBHandler : MonoBehaviour , IVirtualButtonEventHandler {
+
+    public GameObject videoGO;
+
+   
+
+    void Start()
+    {
+        VirtualButtonBehaviour[] vrb = GetComponentsInChildren<VirtualButtonBehaviour>();
+        for (int i = 0; i < vrb.Length; i++)
+        {
+            vrb[i].RegisterEventHandler(this);
+        }
+        videoGO.SetActive(false);
+        GameObject.Find("VideoPlane").SetActive(false);
+        Debug.Log("Plane Deactivated");
+
+
+    }
+
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+       
+            videoGO.SetActive(true);
+        GameObject.Find("VideoPlane").SetActive(true);
+
+        Debug.Log("Video Plane Activated");
+
+       
+
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("virtual button relased");
+    }
+
+}
